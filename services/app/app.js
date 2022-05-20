394102c56aed5489fca1593f95210fdd35dd4b70
Diaz Linggaputra <diazlinggaputra@gmail.com>
2022-05-20 15:45:24 +0700

    merged with socket io

--- services/app/app.js
+++ services/app/app.js
@@ -3,14 +3,12 @@
  * npx sequelize-cli model:generate --name Friend --attributes UserId:integer,fullName:string,email:string,status:boolean
  *
  */
-
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const routes = require("./routes");
 
 const errorHandler = require("./middlewares/errorHandler");
 
@@ -18,8 +16,55 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Socket.io related
+const http = require("http");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST"],
+  },
+});
+
+const routes = require("./routes");
+
 app.use("/", routes);
+
+/* INI BUAT BROADCAST KE SELURUH USER */
+// io.on("connection", (socket) => {
+//   socket.on("send_message", (data) => {
+//     socket.broadcast.emit("receive_message", data);
+//   });
+// });
+//////////
+
+/* INI BUAT JOIN ROOM DAN SEND MESSAGE KE PRIVATE ROOM */
+// io.on("connection", (socket) => {
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//   });
+
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+//   });
+// });
+
+const ChatController = require("./controllers/ChatController");
+
+const chat = io.of("/chat").on("connection", function (socket) {
+  socket.emit("me", socket.id);
+
+  ChatController.sendMessage(chat, socket);
+  ChatController.fetchStranger(chat, socket);
+  ChatController.disconnected(chat, socket);
+  ChatController.videoCallRequest(chat, socket);
+  ChatController.answerCall(chat, socket);
+});
 
 app.use(errorHandler);
 
-module.exports = app;
+module.exports = { server };
---
+++ services/app/controllers/ChatController.js
@@ -0,0 +1,41 @@
+module.exports = {
+  sendMessage: function (endpoint, socket) {
+    /* endpoint output: /chat */
+
+    socket.on("send_message", function (data) {
+      console.log(data);
+
+      socket.to(data.room).emit("receive_message", data);
+    });
+  },
+  fetchStranger: function (endpoint, socket) {
+    // harusnya nanti disini tempat fetch stranger
+
+    socket.on("join_room", (roomNumber) => {
+      socket.join(roomNumber);
+    });
+  },
+  disconnected: function (endpoint, socket) {
+    socket.on("disconnect", (data) => {
+      //   console.log("user disconnect");
+
+      // await delete guest
+      // await update user status
+
+      //   console.log(data);
+      socket.emit("receive_message", "call has been ended");
+    });
+  },
+  videoCallRequest: function (endpoint, socket) {
+    socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+      socket
+        .to(userToCall)
+        .emit("calluser", { signal: signalData, from, name });
+    });
+  },
+  answerCall: function (endpoint, socket) {
+    socket.on("answercall", (data) => {
+      socket.to(data.to).emit("callaccepted", data.signal);
+    });
+  },
+};
