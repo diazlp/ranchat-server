687f0b5d076dfdc7e1c89797f11f0a6475e329e6
mersiera <mersiera18@gmail.com>
2022-05-21 17:48:43 +0700

    change to monolithic

---
+++ services/app/models/guest.js
@@ -0,0 +1,105 @@
+const { ObjectId } = require("mongodb");
+const { getDB } = require("../config/mongoConnection");
+const { generateUsername } = require("unique-username-generator");
+
+class GuestModel {
+  static async findGuestOnline() {
+    try {
+      const db = getDB();
+      const result = await db.collection("Guests").find().toArray();
+
+      return result;
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+
+  static async createGuest() {
+    try {
+      const db = getDB();
+      let num = "";
+      for (let i = 0; i < 4; i++) {
+        num += (Math.floor(Math.random() * 9) + 1).toString();
+      }
+      const guestName = `Guest${generateUsername("", 0, 12)}#${num}`;
+
+      num = "";
+      for (let i = 0; i < 8; i++) {
+        num += (Math.floor(Math.random() * 9) + 1).toString();
+      }
+      const identifier = num;
+      const payload = {
+        guest: guestName,
+        identifier,
+      };
+      const result = await db.collection("Guests").insertOne(payload);
+
+      return result;
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+
+  static async findGuest(id) {
+    try {
+      const db = getDB();
+      const result = await db.collection("Guests").findOne({ _id: ObjectId(id) });
+      console.log("result: ", result);
+      return result;
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+
+  static async deleteGuest(id) {
+    try {
+      const db = getDB();
+      const result = await db.collection("Guests").deleteOne({ _id: ObjectId(id) });
+      return result;
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+
+  static async findRoom() {
+    try {
+      const db = getDB();
+      const result = await db.collection("Rooms").find().toArray();
+      return result;
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+
+  static async createRoom(payload) {
+    try {
+      const db = getDB();
+      const result = await db.collection("Rooms").insertOne(payload);
+      return result;
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+
+  static async connectingGuest({ roomId, guestSocketId }) {
+    try {
+      const db = getDB();
+      const result = await db.collection("Rooms").updateOne({ _id: ObjectId(roomId) }, { $set: { guestCalled: guestSocketId } });
+      return result;
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+
+  static async deleteRoom(id) {
+    try {
+      const db = getDB();
+      const result = await db.collection("Rooms").deleteOne({ _id: ObjectId(id) });
+      return result;
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+}
+
+module.exports = GuestModel;
