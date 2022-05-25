a5790580dd6c34773c539b31f6980287b042bd2d
Diaz Linggaputra <diazlinggaputra@gmail.com>
2022-05-25 16:08:35 +0700

    test deploy

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const MessageController = require("../controllers/MessageController");
+const MessageController = require("../controllers/messageController");
 const requireLogin = require("../middlewares/requireLogin");
+
+const multer = require("multer");
+const upload = multer();
 
 router.use(requireLogin);
 router.post("/roomfriend", MessageController.addRoomFriend); //add RoomFriend
@@ -11,7 +14,11 @@ router.delete(
   MessageController.deleteRoomFriend
 ); //delete RoomFriend
 
-router.post("/addmessage", MessageController.addMessage); //add message
+router.post(
+  "/addmessage",
+  upload.single("image"),
+  MessageController.addMessage
+); //add message
 router.get("/findmessage/:roomfriendid", MessageController.findMessage); //find message
 // router.get("/findlastmessage/:friendid", MessageController.findLastMessage);
 
