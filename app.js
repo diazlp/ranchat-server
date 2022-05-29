04212f453a0500b4db8504d98388e0131eb6757e
Diaz Linggaputra <diazlinggaputra@gmail.com>
2022-05-29 22:34:04 +0700

    try changing cors origin socket io

--- app.js
+++ app.js
@@ -39,10 +39,16 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    credentials: true,
+    origin: "http://localhost:3000",
   },
 });
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//     credentials: true,
+//   },
+// });
 
 /* INI BUAT BROADCAST KE SELURUH USER */
 let users = [];
