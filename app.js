e7cf9e95b750bf28f271ad093226b317723535d4
Diaz Linggaputra <diazlinggaputra@gmail.com>
2022-05-29 22:37:15 +0700

    rechanging origin cors to specific heroku

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://ranchat-app.herokuapp.com",
   },
 });
 
